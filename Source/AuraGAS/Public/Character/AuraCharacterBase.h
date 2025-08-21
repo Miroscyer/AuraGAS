62c7d57ac6c62f9e39fd23794078a431244304f3
Miroscyber <253233345@qq.com>
2025-08-21 15:14:12 +0800

    Initialize Attributes with Gameplay Effects

--- Source/AuraGAS/Public/Character/AuraCharacterBase.h
+++ Source/AuraGAS/Public/Character/AuraCharacterBase.h
@@ -8,6 +8,7 @@
 #include "GameFramework/Character.h"
 #include "AuraCharacterBase.generated.h"
 
+class UGameplayEffect;
 class UAbilitySystemComponent;
 class UAttributeAsset;
 
@@ -34,4 +35,9 @@ protected:
 	TObjectPtr<UAttributeSet> AttributeSet;
 
 	virtual void InitAbilityActorInfo();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Attributes")
+	TSubclassOf<UGameplayEffect> DefaultPrimaryAttributes;
+
+	void InitializePrimaryAttributes() const;
 };

8ab5626bc57047432482a99ba42b86b6cf41c424
Miroscyber <253233345@qq.com>
2025-09-01 22:50:10 +0800

    create Attribute Menu Widget Controller

---
+++ Source/AuraGAS/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+// Copyright Miros
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+	Super::BroadcastInitialValues();
+}
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+	Super::BindCallbacksToDependencies();
+}
